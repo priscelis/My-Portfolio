107571ccc928e535d921edf398aeea51ac907df0
priscelis <prisceliscodrington@gmail.com>
2026-05-07 22:53:02 +0200

    feat: initial project structure and add MainPage

---
+++ src/MainPage.jsx
@@ -0,0 +1,13 @@
+
+
+function MainPage() {
+
+  return (
+    <>
+      <h1>Mi portafolio Personal</h1>
+      <h4>Hola, por lo menos te estaras preguntando, porque esto esta blanco, es porque a penas lo estoy mejorando, loacaba de  abrir</h4>
+    </>
+  )
+}
+
+export default MainPage
