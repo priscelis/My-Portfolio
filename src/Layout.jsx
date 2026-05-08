bce0346466530bb9527e5b68fb6900138ec7cf4a
priscelis <prisceliscodrington@gmail.com>
2026-05-08 15:46:11 +0200

    feat: adding the Layout page

---
+++ src/Layout.jsx
@@ -0,0 +1,10 @@
+import MainPage from "./MainPage"
+
+export default function Layout() {
+  return (
+        <main className=''>
+
+        <MainPage/>8
+        </main>
+  )
+}
--- src/MainPage.jsx
+++ src/MainPage.jsx
@@ -4,8 +4,7 @@ function MainPage() {
 
   return (
     <>
-      <h1>Mi portafolio Personal</h1>
-      <h4>Hola, por lo menos te estaras preguntando, porque esto esta blanco, es porque a penas lo estoy mejorando, loacaba de  abrir</h4>
+      
     </>
   )
 }
