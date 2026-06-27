33693a5fcdd3c378088b0bd0d030a5833914c8ef
priscelis <prisceliscodrington@gmail.com>
2026-06-28 01:36:49 +0200

    WIP: testing navbar underline animations

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,24 @@ import React from 'react'
 
 function Navbar() {
   return (
-    <>Navbar</>
+    <>
+    <nav className='navbarbg bg-amber-500 p-3 h-14 flex justify-between border-blue-700 border-4'>
+    <section>
+      <h2>frfrfr</h2>
+      </section>
+
+      <section className='flex flex-row  justify-end h-13.75 gap-4 align-center items-stretch'>
+     
+        <a href="#"  className="relative inline-block text-black after:content-[''] after:absolute after:left-0 after:bottom-0 after:w-full after:h-[2px] after:bg-purple-600 after:scale-x-0 after:origin-left after:transition-transform after:duration-300 hover:after:scale-x-100">home</a>
+        <a href="#" >home</a>
+        <a href="#">home</a>
+        <a href="#">home</a>
+        <a href="#">home</a>
+      </section>
+
+    </nav>
+    
+    </>
   )
 }
 
