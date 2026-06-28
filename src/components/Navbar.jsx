3f4cc80c821995a1348fd6072b2bc8ab3bc781bb
priscelis <prisceliscodrington@gmail.com>
2026-06-28 22:14:05 +0200

    style(navbar): complete initial navbar styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,19 +3,35 @@ import React from 'react'
 function Navbar() {
   return (
     <>
-    <nav className='navbarbg bg-amber-500 p-3 h-14 flex justify-between border-blue-700 border-4 items-center'>
-    <section>
-      <h2>frfrfr</h2>
-      </section>
+    <nav className='navbarbg bg-[var(--fondo)] p-3 h-14 flex justify-between border-[var(--borde)] border-b-[1.5px] items-center'>
+    <ul>
+     <li>
+        <h2 className='text-2xl font-semibold font-fraunces'>Pris</h2>
+      </li> 
+      </ul>
 
-      <section className='flex flex-row justify-end h-13.75 gap-4 items-center'>
+      <ul className='flex flex-row justify-end h-13.75 gap-4 items-center'>
      
-        <a href="#"  className="relative inline-block text-black after:content-[''] after:absolute after:left-0 after:bottom-0 after:w-full after:h-[2px] after:bg-purple-600 after:scale-x-0 after:origin-left after:transition-transform after:duration-300 hover:after:scale-x-100">Home</a>
-        <a href="#" className="relative inline-block text-black after:content-[''] after:absolute after:left-0 after:bottom-0 after:w-full after:h-[2px] after:bg-purple-600 after:scale-x-0 after:origin-left after:transition-transform after:duration-300 hover:after:scale-x-100">Sobre mi</a>
-        <a href="#" className="relative inline-block text-black after:content-[''] after:absolute after:left-0 after:bottom-0 after:w-full after:h-[2px] after:bg-purple-600 after:scale-x-0 after:origin-left after:transition-transform after:duration-300 hover:after:scale-x-100">Proyectos</a>
-        <a href="#" className="relative inline-block text-black after:content-[''] after:absolute after:left-0 after:bottom-0 after:w-full after:h-[2px] after:bg-purple-600 after:scale-x-0 after:origin-left after:transition-transform after:duration-300 hover:after:scale-x-100">Tecnologias</a>
-        <a href="#" className="relative inline-block text-black after:content-[''] after:absolute after:left-0 after:bottom-0 after:w-full after:h-[2px] after:bg-purple-600 after:scale-x-0 after:origin-left after:transition-transform after:duration-300 hover:after:scale-x-100">Contacto</a>
-      </section>
+        <li>
+          <a href="#"  className=" font-fraunces text-sm relative inline-block text-[var(--text)] after:content-[''] after:rounded-[40px] after:absolute after:left-0 after:bottom-0 after:w-full after:h-[1.5px] after:bg-[var(--borde)] after:scale-x-0 after:origin-left after:transition-transform after:duration-300 hover:after:scale-x-100">Home</a>
+          </li>
+       
+       <li> 
+        <a href="#" className=" font-fraunces text-sm relative inline-block text-[var(--text)] after:content-[''] after:rounded-[40px]   after:absolute after:left-0 after:bottom-0 after:w-full after:h-[1.5px] after:bg-[var(--borde)] after:scale-x-0 after:origin-left after:transition-transform after:duration-300 hover:after:scale-x-100">Sobre mi
+        </a>
+        </li>
+        <li>
+        <a href="#" className=" font-fraunces text-sm relative inline-block text-[var(--text)] after:content-[''] after:rounded-[40px]  after:absolute after:left-0 after:bottom-0 after:w-full after:h-[1.5px] after:bg-[var(--borde)] after:scale-x-0 after:origin-left after:transition-transform after:duration-300 hover:after:scale-x-100">Proyectos</a>
+        </li>
+        
+        <li>
+        <a href="#" className="font-fraunces text-sm relative inline-block text-[var(--text)] after:content-[''] after:rounded-[40px]  after:absolute after:left-0 after:bottom-0 after:w-full after:h-[1.5px] after:bg-[var(--borde)] after:scale-x-0 after:origin-left after:transition-transform after:duration-300 hover:after:scale-x-100">Tecnologias</a>
+        </li>
+
+        <li>
+        <a href="#" className="font-fraunces text-sm relative inline-block text-[var(--text)] after:content-[''] after:absolute after:rounded-[40px] after:left-0 after:bottom-0 after:w-full after:h-[1.5px] after:bg-[var(--borde)] after:scale-x-0 after:origin-left after:transition-transform after:duration-300 hover:after:scale-x-100">Contacto</a>
+        </li>
+      </ul>
 
     </nav>
     
