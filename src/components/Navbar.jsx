eea4dd0e2d8f1dad8fd28a4507395434ab1c4282
priscelis <prisceliscodrington@gmail.com>
2026-07-04 03:36:03 +0200

    refactor(navbar): simplify styles using global defaults

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Navbar() {
   return (
     <>
-    <nav className='navbarbg bg-fondo p-3 h-20 p-8 flex justify-between border-borde border-b-[2.5px] items-center'>
+    <nav className='navbarbg bg-fondo h-20 p-8 flex justify-between border-borde border-b-[2.5px] items-center'>
     <ul>
      <li>
         <h2 className='text-4xl font-normal font-fraunces text-text'>Pris</h2>
@@ -17,19 +17,19 @@ function Navbar() {
           </li> */}
        
        <li> 
-        <a href="#" className=" font-fraunces text-[18px] relative inline-block text-text after:content-[''] after:rounded-[40px] after:absolute after:left-0 after:bottom-0 after:w-full after:h-0.5 after:bg-borde after:scale-x-0 after:origin-left after:transition-transform after:duration-300 hover:after:scale-x-100 ">Sobre mi
+        <a href="#" className="text-[18px] relative inline-block after:content-[''] after:rounded-[40px] after:absolute after:left-0 after:bottom-0 after:w-full after:h-0.5 after:bg-borde after:scale-x-0 after:origin-left after:transition-transform after:duration-300 hover:after:scale-x-100 ">Sobre mi
         </a>
         </li>
         <li>
-        <a href="#" className=" font-fraunces text-[18px] relative inline-block text-text after:content-[''] after:rounded-[40px]  after:absolute after:left-0 after:bottom-0 after:w-full after:h-0.5 after:bg-borde after:scale-x-0 after:origin-left after:transition-transform after:duration-300 hover:after:scale-x-100">Proyectos</a>
+        <a href="#" className="text-[18px] relative inline-block after:content-[''] after:rounded-[40px]  after:absolute after:left-0 after:bottom-0 after:w-full after:h-0.5 after:bg-borde after:scale-x-0 after:origin-left after:transition-transform after:duration-300 hover:after:scale-x-100">Proyectos</a>
         </li>
         
         <li>
-        <a href="#" className="font-fraunces text-[18px] relative inline-block text-text after:content-[''] after:rounded-[40px] after:absolute after:left-0 after:bottom-0 after:w-full after:h-0.5 after:bg-borde after:scale-x-0 after:origin-left after:transition-transform after:duration-300 hover:after:scale-x-100">Tecnologias</a>
+        <a href="#" className="text-[18px] relative inline-block after:content-[''] after:rounded-[40px] after:absolute after:left-0 after:bottom-0 after:w-full after:h-0.5 after:bg-borde after:scale-x-0 after:origin-left after:transition-transform after:duration-300 hover:after:scale-x-100">Tecnologias</a>
         </li>
 
         <li>
-        <a href="#" className="font-fraunces text-[18px] relative inline-block text-text after:content-[''] after:absolute after:rounded-[40px] after:left-0 after:bottom-0 after:w-full after:h-0.5 after:bg-borde after:scale-x-0 after:origin-left after:transition-transform after:duration-300 hover:after:scale-x-100">Contacto</a>
+        <a href="#" className="text-[18px] relative inline-block after:content-[''] after:absolute after:rounded-[40px] after:left-0 after:bottom-0 after:w-full after:h-0.5 after:bg-borde after:scale-x-0 after:origin-left after:transition-transform after:duration-300 hover:after:scale-x-100">Contacto</a>
         </li>
       </ul>
 
