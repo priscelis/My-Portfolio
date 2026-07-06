b0f2ad1af0e0f2eabbb054ffaa1273537833f5fb
priscelis <prisceliscodrington@gmail.com>
2026-07-06 14:16:46 +0200

    feat(footer): finish footer design and layout

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <>
+    <footer className='navbarbg bg-fondo h-20 p-8 flex justify-center border-borde border-t-[2.5px] '>
+   
+        <p className='text-[20px]'>© 2026 Priscelis Codrington</p>
+    </footer>
+    </>
   )
 }
 
